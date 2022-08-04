f4582e85e2c78dca19bb765163bd45b9a87d2d14
Elijah Odjegba <elijahodjegba@gmail.com>
2022-08-04 21:48:13 +0100

    Add single page

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const booksForm = document.querySelector('.books-form');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 
+
 const booksArr = [];
 
 // local storage
@@ -59,3 +60,34 @@ function retrieveShelve() {
   }
 }
 retrieveShelve();
+
+// buttons
+const navList1 = document.getElementById('list')
+const navList2 = document.getElementById('add-new')
+const navList3 = document.getElementById('contact')
+
+// sections
+const sectionOne = document.getElementById('all-book')
+const sectionTwo = document.getElementById('add-new-book')
+const sectionThree = document.getElementById('contact-form')
+
+navList1.addEventListener('click', (e) => {
+  e.preventDefault();
+  sectionOne.classList.remove('hide');
+  sectionTwo.classList.add('hide');
+  sectionThree.classList.add('hide');
+})
+
+navList2.addEventListener('click', (e) => {
+  e.preventDefault();
+  sectionOne.classList.add('hide');
+  sectionTwo.classList.remove('hide');
+  sectionThree.classList.add('hide');
+})
+
+navList3.addEventListener('click', (e) => {
+  e.preventDefault();
+  sectionOne.classList.add('hide');
+  sectionTwo.classList.add('hide');
+  sectionThree.classList.remove('hide');
+})
