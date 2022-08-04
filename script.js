2b54aad814c58a0d2e8979aa809efc8d6ddf10cc
Elijah Odjegba <elijahodjegba@gmail.com>
2022-08-04 14:27:26 +0100

    Fix the input fields

--- script.js
+++ script.js
@@ -1,72 +1,6 @@
-// /* eslint-disable no-unused-vars */
-// const booksContainer = document.querySelector('#books-container');
-// const booksForm = document.querySelector('.books-form');
-// const bookTitle = document.getElementById('title');
-// const bookAuthor = document.getElementById('author');
-
-// const booksArr = [];
-
-// // local storage
-// function booksStorage() {
-//   localStorage.setItem('books', JSON.stringify(booksArr));
-// }
-// // add book to list
-// function addBook(title, author) {
-//   const newBook = {
-//     title,
-//     author,
-//   };
-//   booksArr.push(newBook);
-
-//   const bookItem = document.createElement('div');
-//   booksArr.forEach((book, index) => {
-//     bookItem.innerHTML = `
-//     <div class="book-item">
-//     <p class="book-title">"${book.title}"</p>
-//     <p class="book-author">by ${book.author}</p>
-//     <button type="button" id="${index}" class="remove-btn">Remove</button>                                            
-//     </div>`;
-
-//     const removeBtn = bookItem.querySelector('.remove-btn');
-//     removeBtn.addEventListener('click', () => {
-//       booksArr.splice(index, 1);
-//       bookItem.remove();
-//       booksStorage();
-//     });
-//   });
-//   booksStorage();
-
-//   booksContainer.appendChild(bookItem);
-// }
-
-// // submit button event
-// booksForm.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   if (bookTitle.value !== '' && bookAuthor.value !== '') {
-//     addBook(bookTitle.value, bookAuthor.value);
-
-//     bookTitle.value = '';
-//     bookAuthor.value = '';
-//   }
-// });
-
-// function retrieveShelve() {
-//   const books = JSON.parse(localStorage.getItem('books'));
-//   if (books) {
-//     books.forEach((book) => {
-//       addBook(book.title, book.author);
-//     });
-//   }
-// }
-// retrieveShelve();
-
-
-
-
 // Refactoring using classes
 
 /* eslint-disable max-classes-per-file */
-
 
 class CreateBook {
   constructor(title, author) {
@@ -110,7 +44,7 @@ class CreateBookElements {
     listContainer.className = 'book-item';
     listContainer.innerHTML += `
             <p>"${book.title}" by ${book.author}</p>
-            <button class=".remove-btn">Delete</button>
+            <button class="remove-btn">Delete</button>
             `;
 
     bookContainer.appendChild(listContainer);
@@ -126,25 +60,30 @@ class DisplayBookList {
   }
 
   static removeBook(target) {
-    if (target.classList.contains(".remove-btn")) {
+    if (target.classList.contains('remove-btn')) {
       target.parentElement.remove();
+      LocalStorageClass.removeFromTheStore();
     }
   }
 }
 
 document.addEventListener('DOMContentLoaded', DisplayBookList.displayBooks);
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+
 document.querySelector('.books-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const title = document.getElementById('#title');
-  const author = document.getElementById('#author');
-
   //   create book
-  const book = new CreateBook(title, author);
+  const book = new CreateBook(title.value, author.value);
+
+  CreateBookElements.createBookElement(book);
+
   // add it to local storage
   LocalStorageClass.addbookToStore(book);
   // append the book to the book list
-  CreateBookElements.createBookElement(book);
+
   //  Reseting the form inputs
   const form = document.querySelector('.books-form');
   form.reset();
@@ -157,24 +96,7 @@ document.querySelector('#books-container').addEventListener('click', (e) => {
   DisplayBookList.removeBook(e.target);
 
   // remove book from the store
-  LocalStorageClass.removeFromTheStore(e.target.parentElement.firstElementChild.textContent);
+  LocalStorageClass.removeFromTheStore(
+    e.target.parentElement.firstElementChild.textContent,
+  );
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
